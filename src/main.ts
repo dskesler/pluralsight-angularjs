71df874696191d659b7839fdace115150afd8d72
dskesler <skesler@ebsco.com>
2017-02-10 13:51:43 -0600

    removed gulpfile

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-alert("ok");
+// Code goes here
+import {angular} from 'angular';
 
+(function() {
+
+  var app = angular.module("githubViewer", []); //need empty array to declare module
+
+  var MainController = function ($scope, $http){
+  var onUserComplete = function(response){
+    $scope.user = response.data;
+    var imageSource = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F564x%2Fa3%2F5e%2F81%2Fa35e812958d649f2a427a4af4b5206b2.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F469500329874878608%2F&docid=--M8rfXrejNJlM&tbnid=9qhhvtvtJNhpIM%3A&vet=1&w=500&h=415&bih=1201&biw=1280&q=moose%20stuffed%20animal&ved=0ahUKEwjU0LyOlubRAhXBRiYKHXKqC6IQMwiLASgQMBA&iact=mrc&uact=8;';
+  }
+
+  var onError = function(reason) {
+
+    $scope.error = "Could not fetch the user";
+
+  };
+
+  $scope.search = function(userName){
+     $http.get("https://api.github.com/users/" + userName)
+  .then(onUserComplete, onError);
+  }
+
+
+  $scope.message= "Hello";
+};
+
+  app.controller("MainController", MainController);
+}());
