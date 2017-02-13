3e2e01c9f9eb6ae3aaeff49ab7d7fd361a99745e
dskesler <skesler@ebsco.com>
2017-02-13 10:23:44 -0600

    switched back to using JS

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,30 @@
 import * as angular from 'angular';
 
-(function(){
-  var app= angular.module("githubViewer", []); //need empty array to declare module
+(function () {
 
-  var MainController = function ($scope, $http){
-  var onUserComplete = function(response){
-    $scope.user = response.data;
-    var imageSource = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F564x%2Fa3%2F5e%2F81%2Fa35e812958d649f2a427a4af4b5206b2.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F469500329874878608%2F&docid=--M8rfXrejNJlM&tbnid=9qhhvtvtJNhpIM%3A&vet=1&w=500&h=415&bih=1201&biw=1280&q=moose%20stuffed%20animal&ved=0ahUKEwjU0LyOlubRAhXBRiYKHXKqC6IQMwiLASgQMBA&iact=mrc&uact=8;';
-  }
+  //creating our new module
+  var app = angular.module("githubViewer", []); //need empty array to declare module
 
-  var onError = function(reason) {
-    $scope.error = "Could not fetch the user";
+  var GithubViewer = function ($scope, $http) {
+    var onUserComplete = function (response) {
+      $scope.user = response.data;
+      var imageSource = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F564x%2Fa3%2F5e%2F81%2Fa35e812958d649f2a427a4af4b5206b2.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F469500329874878608%2F&docid=--M8rfXrejNJlM&tbnid=9qhhvtvtJNhpIM%3A&vet=1&w=500&h=415&bih=1201&biw=1280&q=moose%20stuffed%20animal&ved=0ahUKEwjU0LyOlubRAhXBRiYKHXKqC6IQMwiLASgQMBA&iact=mrc&uact=8;';
+    }
+
+    var onError = function (reason) {
+      $scope.error = "Could not fetch the user";
+    };
+
+    $scope.search = function (userName) {
+      $http.get("https://api.github.com/users/" + userName)
+        .then(onUserComplete, onError);
+    }
+
+
+    $scope.message = "Hello";
   };
 
-  $scope.search = function(userName){
-     $http.get("https://api.github.com/users/" + userName)
-  .then(onUserComplete, onError);
-  }
+  //registering controller: ("Name of controller", Function_To_Use_For_Controller)
+  app.controller("GithubViewer", ["$scope", "$http", GithubViewer]);
 
-
-  $scope.message= "Hello";
-};
-
-  app.controller("MainController", MainController);
-
-  })();
+})();
